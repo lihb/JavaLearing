acb310cf1622f8efdb09aab24dae6052ead94cd1
lihb <liqixi_169@126.com>
2014-03-14 18:23:43 +0800

    newLine() method

--- SocketDemo.java
+++ SocketDemo.java
@@ -3,9 +3,10 @@
  *File:     SocketDemo.java
  *Date:     14-03-14 16:13:10
  *客户端输入任意的字符，服务端将其转换成大写，返回给给客户端。
- *注意：在用BufferedWriter和BufferedoutPutStream写入数据后，须调用flush()方法将数据写出。
- *      那是否都一定要调用flush()方法呢，答案是：不一定。只要你能保证你写的所有数据都能大于8KB。
- *      如不能保证，请调用flush()方法。
+ *注意：1.在用BufferedWriter和BufferedoutPutStream写入数据后，须调用flush()方法将数据写出。
+ *		  那是否都一定要调用flush()方法呢，答案是：不一定。只要你能保证你写的所有数据都能大于8KB。
+ *        如不能保证，请调用flush()方法。
+ *      2.BufferedWriter特有的newLine()方法，产生一个换行标记，否则BufferedReader的readLine()方法读不到换行标记，会一直在那等着。
  *总结：谨慎起见，还是调用flush()方法为妙，另close()方法也会调用flush()方法。
  *详情参见：http://blog.csdn.net/veryitman/article/details/6460726    
  */
